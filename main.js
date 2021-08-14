47c861e91e61b9a58546d57422a63421b8cac157
PrintRonald <madariaga492@gmail.com>
2021-08-14 14:15:07 -0400

    varsiion 14 agosto
    cambiendod estilo

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-var resultado = window.confirm('Estas seguro?');
-if (resultado === true) {
-    window.alert('BIENVENIDO A MI SITIO WEB');
-} else { 
-    window.alert('Pareces indeciso');
+// var resultado = window.confirm("Estas seguro?");
+// if (resultado === true) {
+//   window.alert("BIENVENIDO A MI SITIO WEB");
+// } else {
+//   window.alert("Pareces indeciso");
+// }
+
+function changeView(type) {
+  if (type === "HOME") {
+    const path = window.location.pathname;
+    if (path === "/index.html") {
+      document.body.style.backgroundColor = "red";
+      return false;
+    } else {
+      window.location.href = "index.html";
+    }
+  } else if (type === "REGISTRO") {
+    window.location.href = "registro.html";
+  } else {
+    window.location.href = "rutas.html";
+  }
 }
